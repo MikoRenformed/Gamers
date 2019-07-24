010a223cf7149ce326680f9329ba8533e5f5810c
sako0518 <46761406+sako0518@users.noreply.github.com>
2019-07-24 17:02:42 -0600

    Update and rename addGame (1).sql to addGame.sql

--- addGame.sql
+++ addGame.sql
@@ -9,8 +9,8 @@ var con = mysql.createConnection({ --connects to server, change values to w/e se
             
 con.connect(function(err) { --add a game
   if (err) throw err;
-  var sql = "INSERT INTO GameInfo(GameName,ConferenceShown,Genre,Developer,Publisher,ReleaseDate,MetacriticScore,Info) VALUES (Gname,Gconference,GGenre,Gdev,Gpub,Grelease,Gscore,Ginfo)";
-  con.query(sql, function (err, result) { --again, get all values from some text box
+  var sql = "INSERT INTO GameInfo(GameName,ConferenceShown,Genre,Developer,Publisher,ReleaseDate,MetacriticScore,Info) VALUES (?,?,?,?,?,?,?,?)";
+  con.query(sql,[Gname,Gconference,GGenre,Gdev,Gpub,Grelease,Gscore,Ginfo], function (err, result) { 
     if (err) throw err;
   });
 });
