3c5ed8dccd5d10e4dc0d9076cce5bebfe82fb469
sako0518 <46761406+sako0518@users.noreply.github.com>
2019-07-24 16:59:58 -0600

    Update and rename addUser (1).sql to addUser.sql

--- addUser.sql
+++ addUser.sql
@@ -9,8 +9,8 @@ var con = mysql.createConnection({ --connects to server, change values to w/e se
 
 con.connect(function(err) { --add a user account
   if (err) throw err;
-  var sql = "INSERT INTO userinfo(firstname,lastname,email,password) VALUES (Ufname,Ulname,Uemail,Upass)"; --the 4 values must be input by user, so we need box inputs accordingly
-  con.query(sql, function (err, result) {
+  var sql = "INSERT INTO userinfo(firstname,lastname,email,password) VALUES (?,?,?,?)";
+  con.query(sql,[Ufname,Ulname,Uemail,Upass], function (err, result) { --Ufname etc must be gathered by the user
     if (err) throw err;
   });
 });
